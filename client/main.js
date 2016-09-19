c12457db50a529d81942dab1d7d919bf48270c03
Lespagnol Rémy <remy.lespagnol@gmail.com>
2016-09-19 22:44:47 +0200

    Add auth module

--- client/main.js
+++ client/main.js
@@ -4,6 +4,7 @@ import initContext from './configs/context';
 // modules
 import coreModule from './modules/core';
 import layoutModule from './modules/layout';
+import authModule from './modules/auth';
 
 // init context
 const context = initContext();
@@ -12,4 +13,5 @@ const context = initContext();
 const app = createApp(context);
 app.loadModule(coreModule);
 app.loadModule(layoutModule);
+app.loadModule(authModule);
 app.init();
